15fb82d317a1eb3e5807b3bf95eb6195d1ca2bd6
Byron2016 <bgva2005@yahoo.com>
2024-07-03 17:47:31 -0500

    chore: add linter dev dependency and configure's files

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+const RULES = {
+  OFF: 'off',
+  WARN: 'warn',
+  ERROR: 'error',
+}
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['standard', 'prettier'],
+  ignorePatterns: ['dist', '.eslintrc.js'],
+  settings: {},
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [],
+  rules: {},
+}
